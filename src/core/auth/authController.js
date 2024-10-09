9a1f699ebb7d39d321282a98449f543896fea2d7
Santiago00001 <santiagoarango0214@gmail.com>
2024-10-09 16:56:20 -0500

    Update authController.js

--- src/core/auth/authController.js
+++ src/core/auth/authController.js
@@ -11,14 +11,14 @@ const login = async (req, res) => {
     }
 
     try {
-        // Busca el usuario por nombre de usuario
+        // Busca el usuario por correo
         const user = await User.findOne({ correo });
         if (!user) {
             return res.status(401).json({ message: 'Usuario no encontrado.' });
         }
 
         // Verifica la contraseña (suponiendo que usas bcrypt para hashear las contraseñas)
-        const isMatch = await user.comparePassword(password); // Define este método en tu modelo User
+        const isMatch = await user.comparePassword(password); // Asegúrate de tener este método en tu modelo User
         if (!isMatch) {
             return res.status(401).json({ message: 'Contraseña incorrecta.' });
         }
@@ -26,8 +26,8 @@ const login = async (req, res) => {
         // Genera un token JWT
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '5h' });
 
-        // Envía el token como respuesta
-        res.status(200).json({ token });
+        // Envía el token y el ID del usuario como respuesta
+        res.status(200).json({ token, userId: user._id });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error en el servidor.' });
